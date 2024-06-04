0369dbb8a85b7cecb20a1f12934452711cb351fc
Kyuchul Lee <40943064+kyugorithm@users.noreply.github.com>
2024-06-04 17:47:04 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,22 @@
-	1.	Sports Events: Besides baseball, other sports like soccer, basketball, and rugby often display logos on stadiums, uniforms, equipment, etc.
-	2.	Auto Racing: Events like Formula 1, NASCAR, or rally races are rich in logos on cars, driver outfits, and track billboards.
-	3.	Music Festivals and Concerts: Logos of sponsors and brands are commonly displayed on stage backdrops, equipment, and advertising banners.
-	4.	Commercial Advertisements: TV commercials or YouTube ad clips intensely showcase product and brand logos.
-	5.	Fashion Shows: Runway events or fashion-related videos prominently feature designer and brand logos on clothing, accessories, and backgrounds.
-	6.	Tech Expos: Large-scale tech exhibitions like CES or MWC display various technology brand logos on exhibition booths, products, and promotional materials.
-	7.	News Broadcasts: Logos in news studios, tickers, and lower thirds are also good sources of data.
+# Parse base options
+options = BaseOptions()
+opts = options.parse()
+
+# Select mode and corresponding YAML input or model path
+yaml_input = f"yolov10_{opts.model_size}.yaml"
+model_input = yaml_input if (opts.mode == "train" and not opts.resume) else opts.model_path
+model = YOLO(model_input)
+
+# Initialize model based on selected mode
+print(f"Selected Mode: {opts.mode}")
+print("Model Setting Completed. Start the mode.\n")
+
+# Execute operation based on mode
+if opts.mode == "train":
+    model.train(data=opts.train_dataset, epochs=opts.epochs)
+elif opts.mode == "validation":
+    metrics = model.val()
+elif opts.mode == "test":
+    results = model(opts.video_path)
+elif opts.mode == "track":
+    model.track(opts.video_path)
