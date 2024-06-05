9ec4a99cc26d1d44033e51c2fb543e6249788d51
Kyuchul Lee <40943064+kyugorithm@users.noreply.github.com>
2024-06-05 22:13:20 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,27 +1,30 @@
-<?xml version="1.0" ?><annotation verified="no">
-	<folder>VOC2007</folder>
-	<filename>1.jpg</filename>
-	
-	<source>
-		<database>Unknown</database>
-	</source>
-	<size>
-		<width>484</width>
-		<height>383</height>
-		<depth>3</depth>
-	</size>
-	<segmented>0</segmented>
-	<object>
-		<name>2xist</name>
-		<pose>Unspecified</pose>
-		<truncated>0</truncated>
-		<difficult>0</difficult>
-		<bndbox>
-			<xmin>276</xmin>
-			<ymin>143</ymin>
-			<xmax>459</xmax>
-			<ymax>184</ymax>
-		</bndbox>
-	</object>
-</annotation>
+import xml.etree.ElementTree as ET
 
+def extract_bndbox_details(file_path):
+    # XML 파일 파싱
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    
+    # bndbox 요소 찾기
+    bndbox = root.find('.//bndbox')
+    
+    # xmin, ymin, xmax, ymax 값 추출
+    xmin = int(bndbox.find('xmin').text)
+    ymin = int(bndbox.find('ymin').text)
+    xmax = int(bndbox.find('xmax').text)
+    ymax = int(bndbox.find('ymax').text)
+    
+    # 너비와 높이 계산
+    w = xmax - xmin
+    h = ymax - ymin
+    
+    # 중심점 계산
+    xc = xmin + w / 2
+    yc = ymin + h / 2
+    
+    return xc, yc, w, h
+
+# 사용 예시
+file_path = 'path/to/your/xmlfile.xml'
+xc, yc, w, h = extract_bndbox_details(file_path)
+print(f'xc: {xc}, yc: {yc}, w: {w}, h: {h}')
