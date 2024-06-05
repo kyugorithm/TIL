af25195966a4c14733daadb4e5313344cadfdfba
Kyuchul Lee <40943064+kyugorithm@users.noreply.github.com>
2024-06-05 22:10:41 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,27 @@
-https://drive.google.com/file/d/1f5PpFNj5_c2k8xIfn_VMjKrLRpXirGBI/view?usp=sharing
+<?xml version="1.0" ?><annotation verified="no">
+	<folder>VOC2007</folder>
+	<filename>1.jpg</filename>
+	
+	<source>
+		<database>Unknown</database>
+	</source>
+	<size>
+		<width>484</width>
+		<height>383</height>
+		<depth>3</depth>
+	</size>
+	<segmented>0</segmented>
+	<object>
+		<name>2xist</name>
+		<pose>Unspecified</pose>
+		<truncated>0</truncated>
+		<difficult>0</difficult>
+		<bndbox>
+			<xmin>276</xmin>
+			<ymin>143</ymin>
+			<xmax>459</xmax>
+			<ymax>184</ymax>
+		</bndbox>
+	</object>
+</annotation>
+
