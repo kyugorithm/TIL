24cc152caa19f4b5e00c65679bcb4c3b84aac5f3
Kyuchul Lee <40943064+kyugorithm@users.noreply.github.com>
2024-07-10 09:27:28 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,1 @@
-import time
-import random
-from script import get_image_from_google, get_languages
-
-def get_images_with_delay(keyword, save_path, max_num, min_delay=1, max_delay=5):
-    for _ in range(max_num):
-        get_image_from_google(keyword, save_path=save_path, max_num=1)
-        delay = random.uniform(min_delay, max_delay)
-        time.sleep(delay)
-
-# 사용 예시
-get_images_with_delay("Apple", save_path="temp/Apple", max_num=50)
+https://drive.google.com/file/d/1Cik27Ztoc7SbI4dHYl7OOYIjuIO38djU/view?usp=sharing
