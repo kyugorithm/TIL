782201e285f5ad8c3dd276271a7a66d86a6e1440
Kyuchul Lee <lkc880425@gmail.com>
2024-12-03 00:26:25 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,3 @@
 
+ENV LD_LIBRARY_PATH=/usr/local/lib/python3.10/dist-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH
+ENV C_INCLUDE_PATH=/usr/local/lib/python3.10/dist-packages/nvidia/cudnn/include:$C_INCLUDE_PATH
