7ec4c6c940e60223458b62a3996683ff58b8342c
Kyuchul Lee <lkc880425@gmail.com>
2025-01-16 23:22:21 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,14 @@
-import cv2
-import numpy as np
-
-def adjust_gamma(image, gamma=1.0):
-    inv_gamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** inv_gamma) * 255
-        for i in np.arange(0, 256)]).astype("uint8")
-    return cv2.LUT(image, table)
-
-# gamma < 1: 어두운 영역이 밝아짐
-# gamma > 1: 밝은 영역이 어두워짐
-brightened = adjust_gamma(image, gamma=0.5)
+def increase_vibrance(image, factor=1.3):
+    # HSV 변환
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    
+    # 낮은 채도 영역에 더 큰 가중치 적용
+    mask = (255 - s) / 255.0
+    adjustment = s * (1 + factor * mask)
+    
+    # 범위 제한
+    s = np.clip(adjustment, 0, 255).astype('uint8')
+    
+    hsv = cv2.merge([h, s, v])
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
