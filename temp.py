2adf4be6f5f5013c868449df1f134fee93493ff2
Kyuchul Lee <lkc880425@gmail.com>
2025-02-04 20:56:43 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,26 +1,13 @@
-import boto3
-import json
-
-# Lambda 클라이언트 생성
-lambda_client = boto3.client('lambda')
-
-# Lambda 함수 호출할 데이터 준비
-payload = {
-   "key1": "value1",
-   "key2": "value2"
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::123456789012:role/SageMaker-ExecutionRole"
+            },
+            "Action": "lambda:InvokeFunction",
+            "Resource": "arn:aws:lambda:region:123456789012:function:YourFunctionName"
+        }
+    ]
 }
-
-try:
-   # Lambda 함수 호출 - ARN 사용 및 동기식
-   response = lambda_client.invoke(
-       FunctionName='arn:aws:lambda:region:account-id:function:function-name',  # 실제 Lambda ARN으로 교체
-       InvocationType='RequestResponse',  # 동기식 호출
-       Payload=json.dumps(payload)
-   )
-   
-   # 응답 처리
-   response_payload = json.loads(response['Payload'].read().decode('utf-8'))
-   print("Lambda 함수 응답:", response_payload)
-   
-except Exception as e:
-   print("Error:", str(e))
